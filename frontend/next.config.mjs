f3fd057d83be0b861a8cb4cc6473945dbceda866
ABfry <is0605ek@ed.ritsumei.ac.jp>
2026-07-13 18:00:17 +0900

    fix: next.config.tsを.mjs化しhealthエンドポイントを追加

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,10 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
 const cdnDomain = process.env.NEXT_PUBLIC_CDN_URL
   ? process.env.NEXT_PUBLIC_CDN_URL.split('//')[1]
   : '202502-test-bucket.s3.ap-northeast-1.amazonaws.com';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com', cdnDomain, 'lh3.googleusercontent.com'],
